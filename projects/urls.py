6dc6b8771bdc253fc64782c5be8ef1be89423d79
TessieWaithira <terrywaithira1@gmail.com>
2017-03-23 11:20:58 +0000

    create a project_list view

--- projects/urls.py
+++ projects/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.HomePageView.as_view()),
+    url(r'^$', views.project_list, name='project_list'),
 ]
--- projects/views.py
+++ projects/views.py
@@ -5,3 +5,7 @@ from django.views.generic import TemplateView
 class HomePageView(TemplateView):
     def get(self, request):
         return render(request, 'index.html')
+
+def project_list(request):
+    return render(request, 'project/project_list.html', {})
+
