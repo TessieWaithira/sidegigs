772df2d5beaede9ae37ac5dc659e03f613c3d8c5
TessieWaithira <terrywaithira1@gmail.com>
2017-03-23 16:41:08 +0000

    create a form for Project model

--- projects/urls.py
+++ projects/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     url(r'^$', views.HomePageView.as_view()),
     url(r'^project_list/', views.project_list, name='project_list'),
+    url(r'^project_new/', views.project_new, name='project_new'),
 ]
--- projects/views.py
+++ projects/views.py
@@ -3,6 +3,7 @@ from django.shortcuts import redirect
 from django.views.generic import TemplateView
 from .models import Project
 from django.utils import timezone
+from .forms import ProjectForm
 # Create your views here.
 class HomePageView(TemplateView):
     def get(self, request):
@@ -11,4 +12,8 @@ class HomePageView(TemplateView):
 def project_list(request):
     projects = Project.objects.filter(created_date__lte=timezone.now()).order_by('created_date')
     return render(request, 'project_list.html', {'projects': projects})
+
+def project_new(request):
+    form = ProjectForm()
+    return render(request, 'project_new.html', {'form': form})
 
