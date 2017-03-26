e5da0ae2a42e0f3aae12a9f88a7bb9108451c960
TessieWaithira <terrywaithira1@gmail.com>
2017-03-26 22:40:18 +0000

    some changes

--- gigs/wsgi.py
+++ gigs/wsgi.py
@@ -1,12 +1,3 @@
-"""
-WSGI config for gigs project.
-
-It exposes the WSGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/1.10/howto/deployment/wsgi/
-"""
-
 import os
 
 from django.core.wsgi import get_wsgi_application
@@ -15,4 +6,5 @@ os.environ.setdefault("DJANGO_SETTINGS_MODULE", "gigs.settings")
 
 application = get_wsgi_application()
 from whitenoise.django import DjangoWhiteNoise
+
 application = DjangoWhiteNoise(application)
