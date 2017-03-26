0fb4b6f2dfe7e2b478d71848e09ab4877d6a2bcf
TessieWaithira <terrywaithira1@gmail.com>
2017-03-26 12:26:59 +0000

    create a template link to a project detail

--- projects/urls.py
+++ projects/urls.py
@@ -3,6 +3,7 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.HomePageView.as_view()),
-    url(r'^project_list/', views.project_list, name='project_list'),
-    url(r'^project_new/', views.project_new, name='project_new'),
+    url(r'^project_list/$', views.project_list, name='project_list'),
+    url(r'^project/(?P<pk>\d+)/$', views.project_detail, name='project_detail'),
+    url(r'^project_new/$', views.project_new, name='project_new'),
 ]
--- projects/views.py
+++ projects/views.py
@@ -1,19 +1,35 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.shortcuts import redirect
 from django.views.generic import TemplateView
 from .models import Project
 from django.utils import timezone
 from .forms import ProjectForm
+
+
 # Create your views here.
 class HomePageView(TemplateView):
     def get(self, request):
         return render(request, 'index.html')
 
+
 def project_list(request):
-    projects = Project.objects.filter(created_date__lte=timezone.now()).order_by('created_date')
+    projects = Project.objects.all()
     return render(request, 'project_list.html', {'projects': projects})
 
-def project_new(request):
-    form = ProjectForm()
-    return render(request, 'project_new.html', {'form': form})
 
+def project_detail(request, pk):
+    project = get_object_or_404(Project, pk=pk)
+    return render(request, 'project_detail.html', {'project': project})
+
+
+def project_new(request):
+    if request.method == "POST":
+        form = ProjectForm(request.POST)
+        if form.is_valid():
+            project = form.save(commit=False)
+            project.project_owner = request.user
+            project.save()
+            return redirect('project_list.html')
+    else:
+        form = ProjectForm()
+    return render(request, 'project_new.html', {'form': form})
